7409bedc3cec7d1479b66522ede2a67468a17c0f
Mohit Jindal <ZJINMOH@ericsson.com>
2019-06-17 15:15:11 +0530

    SpringBootJdbcHikariCP

---
+++ src/main/java/com/infotech/app/SpringBootJdbcHikariCPApplication.java
@@ -0,0 +1,52 @@
+package com.infotech.app;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.infotech.app.dao.EmployeeDAO;
+import com.infotech.app.model.Employee;
+
+@SpringBootApplication
+public class SpringBootJdbcHikariCPApplication implements CommandLineRunner {
+
+	@Autowired
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcHikariCPApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//createEmployee();
+		getEmployeeById();
+		//employeeDAO.updateEmployeeEmailById(1, "update@gmail.com");
+		//employeeDAO.deleteEmployeeById(1);
+		System.out.println("DataSource:" + dataSource);
+	}
+
+	private void getEmployeeById()
+	{
+		Employee employee = employeeDAO.getEmployeeById(2);
+		System.out.println(employee);
+	}
+	
+	private void createEmployee()
+	{
+		Employee employee = new Employee();
+		employee.setEmployeeName("Kishan");
+		employee.setEmail("kisan.s@gmail.com");
+		employee.setSalary(90000.00);
+		
+		employeeDAO.createEmployee(employee);
+	}
+}
